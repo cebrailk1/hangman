371ae756e136b6c7662d4b5439adbce1332949d6
Cebrail Kocakafa <cebrail.kocakafa@telefonica.com>
2024-09-29 10:48:52 +0200

    stylingupdates

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 // import { generate, count } from "random-words";
-
+// let restartButton = document.getElementById("button");
+// restartButton.onclick = console.log("Button pressed");
 function hangoman() {}
 // let randomWords = (generate())
-let winner = "telekommunikationsüberwachungsverordnung";
+let healthSymbol = document.querySelector("#health");
+let winner = "zauberstab";
 let winnerChars = winner.split("");
 let array = [];
 for (let i = 0; i < winnerChars.length; i++) {
   array.push("_");
 }
+let health2 = document.querySelector("#health2");
+let restartButton2 = document.querySelector("#restartButton");
 let keyPress = "H";
 let result = winnerChars.filter((key) => key == keyPress);
 let health = 8;
@@ -31,12 +35,41 @@ function logKey(event) {
     }
   }
   if (health == 0) {
-    return "GAMEOVER"
+    return "GAMEOVER";
   }
 
   if (!winnerChars.includes(event.key)) {
     console.log((health -= 1));
+  
+  }   
+
+  if (health == 7) {
+    healthSymbol.innerHTML =
+      "&hearts;&hearts;&hearts;&hearts;&hearts;&hearts;&hearts;";
   }
+  if (health == 6) {
+    healthSymbol.innerHTML = "&hearts;&hearts;&hearts;&hearts;&hearts;&hearts;";
+  }
+  if (health == 5) {
+    healthSymbol.innerHTML = "&hearts;&hearts;&hearts;&hearts;&hearts;";
+  }
+  if (health == 4) {
+    healthSymbol.innerHTML = "&hearts;&hearts;&hearts;&hearts;";
+  }
+  if (health == 3) {
+    healthSymbol.innerHTML = "&hearts;&hearts;&hearts;";
+  }
+  if (health == 2) {
+    healthSymbol.innerHTML = "&hearts;&hearts;";
+  }
+  if (health == 1) {
+    healthSymbol.innerHTML = "&hearts;";
+  }
+  if (health == 0) {
+    healthSymbol.innerHTML = "&#9760;";
+    healthSymbol.style.fontSize = "100px";
+  }
+
   if (health >= 1) {
     win.textContent = "noch " + health + " Leben";
   }
