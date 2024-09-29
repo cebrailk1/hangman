31648117acc0e5f0688da096bd18d4453e80d152
Cebrail Kocakafa <cebrail.kocakafa@telefonica.com>
2024-09-29 11:04:13 +0200

    startbutton

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-// import { generate, count } from "random-words";
-// let restartButton = document.getElementById("button");
-// restartButton.onclick = console.log("Button pressed");
-function hangoman() {}
-// let randomWords = (generate())
+let button = document.querySelector(".startButton");
+
+button.addEventListener("click", function () {
+
 let healthSymbol = document.querySelector("#health");
 let winner = "zauberstab";
 let winnerChars = winner.split("");
@@ -27,56 +26,57 @@ const body = document.querySelector("body");
 
 body.addEventListener("keypress", logKey);
 
-function logKey(event) {
-  for (let i = 0; i < winnerChars.length; i++) {
-    if (event.key === winnerChars[i]) {
-      array[i] = winnerChars[i];
-      gameWord.textContent = array.join(" ");
+  function logKey(event) {
+    for (let i = 0; i < winnerChars.length; i++) {
+      if (event.key === winnerChars[i]) {
+        array[i] = winnerChars[i];
+        gameWord.textContent = array.join(" ");
+      }
+    }
+    if (health == 0) {
+      return "GAMEOVER";
+    }
+
+    if (!winnerChars.includes(event.key)) {
+      console.log((health -= 1));
+    }
+
+    if (health == 7) {
+      healthSymbol.innerHTML =
+        "&hearts;&hearts;&hearts;&hearts;&hearts;&hearts;&hearts;";
+    }
+    if (health == 6) {
+      healthSymbol.innerHTML =
+        "&hearts;&hearts;&hearts;&hearts;&hearts;&hearts;";
+    }
+    if (health == 5) {
+      healthSymbol.innerHTML = "&hearts;&hearts;&hearts;&hearts;&hearts;";
+    }
+    if (health == 4) {
+      healthSymbol.innerHTML = "&hearts;&hearts;&hearts;&hearts;";
+    }
+    if (health == 3) {
+      healthSymbol.innerHTML = "&hearts;&hearts;&hearts;";
+    }
+    if (health == 2) {
+      healthSymbol.innerHTML = "&hearts;&hearts;";
+    }
+    if (health == 1) {
+      healthSymbol.innerHTML = "&hearts;";
+    }
+    if (health == 0) {
+      healthSymbol.innerHTML = "&#9760;";
+      healthSymbol.style.fontSize = "100px";
+    }
+
+    if (health >= 1) {
+      win.textContent = "noch " + health + " Leben";
+    }
+    if (health < 1) {
+      win.textContent = "verloren!";
     }
   }
-  if (health == 0) {
-    return "GAMEOVER";
+  if (array.join("") == winnerChars.join("")) {
+    win.textContent = "Gewonnen!";
   }
-
-  if (!winnerChars.includes(event.key)) {
-    console.log((health -= 1));
-  
-  }   
-
-  if (health == 7) {
-    healthSymbol.innerHTML =
-      "&hearts;&hearts;&hearts;&hearts;&hearts;&hearts;&hearts;";
-  }
-  if (health == 6) {
-    healthSymbol.innerHTML = "&hearts;&hearts;&hearts;&hearts;&hearts;&hearts;";
-  }
-  if (health == 5) {
-    healthSymbol.innerHTML = "&hearts;&hearts;&hearts;&hearts;&hearts;";
-  }
-  if (health == 4) {
-    healthSymbol.innerHTML = "&hearts;&hearts;&hearts;&hearts;";
-  }
-  if (health == 3) {
-    healthSymbol.innerHTML = "&hearts;&hearts;&hearts;";
-  }
-  if (health == 2) {
-    healthSymbol.innerHTML = "&hearts;&hearts;";
-  }
-  if (health == 1) {
-    healthSymbol.innerHTML = "&hearts;";
-  }
-  if (health == 0) {
-    healthSymbol.innerHTML = "&#9760;";
-    healthSymbol.style.fontSize = "100px";
-  }
-
-  if (health >= 1) {
-    win.textContent = "noch " + health + " Leben";
-  }
-  if (health < 1) {
-    win.textContent = "verloren!";
-  }
-}
-if (array.join("") == winnerChars.join("")) {
-  win.textContent = "Gewonnen!";
-}
+});
