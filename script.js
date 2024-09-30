993d4d9d96f7140a9f33fcc7cc6a43654d555c2e
Cebrail Kocakafa <cebrail.kocakafa@telefonica.com>
2024-09-30 10:20:28 +0200

    updates

--- script.js
+++ script.js
@@ -1,30 +1,38 @@
 let button = document.querySelector(".startButton");
 
 button.addEventListener("click", function () {
+  let healthSymbol = document.querySelector("#health");
+  let winner = "zauberstab";
+  let winnerChars = winner.split("");
+  let array = [];
+  for (let i = 0; i < winnerChars.length; i++) {
+    array.push("_");
+  }
+  let health2 = document.querySelector("#health2");
+  let restartButton2 = document.querySelector("#restartButton");
+  let keyPress = "H";
+  let result = winnerChars.filter((key) => key == keyPress);
+  let gameWord = document.querySelector("#gameWord");
+  let win = document.querySelector("#win");
+  let keyboard = document.querySelector("#keyboard");
+  console.log(winnerChars);
+  console.log(keyPress);
+  console.log(result);
 
-let healthSymbol = document.querySelector("#health");
-let winner = "zauberstab";
-let winnerChars = winner.split("");
-let array = [];
-for (let i = 0; i < winnerChars.length; i++) {
-  array.push("_");
-}
-let health2 = document.querySelector("#health2");
-let restartButton2 = document.querySelector("#restartButton");
-let keyPress = "H";
-let result = winnerChars.filter((key) => key == keyPress);
-let health = 8;
-let gameWord = document.querySelector("#gameWord");
-let win = document.querySelector("#win");
-console.log(winnerChars);
-console.log(keyPress);
-console.log(result);
+  gameWord.textContent = array.join(" ");
 
-gameWord.textContent = array.join(" ");
+  const body = document.querySelector("body");
 
-const body = document.querySelector("body");
-
-body.addEventListener("keypress", logKey);
+  let healthCount = "&hearts;";
+  let skull = "&#9760;";
+  let health = 8;
+  if ((health = 8)) {
+    healthSymbol.style.fontSize = "25px";
+  }
+  if (health == 8) {
+    healthSymbol.innerHTML = healthCount.repeat(health);
+  }
+  body.addEventListener("keypress", logKey);
 
   function logKey(event) {
     for (let i = 0; i < winnerChars.length; i++) {
@@ -33,6 +41,10 @@ body.addEventListener("keypress", logKey);
         gameWord.textContent = array.join(" ");
       }
     }
+
+    keyboard.textContent = event.key;
+
+
     if (health == 0) {
       return "GAMEOVER";
     }
@@ -40,33 +52,31 @@ body.addEventListener("keypress", logKey);
     if (!winnerChars.includes(event.key)) {
       console.log((health -= 1));
     }
-
     if (health == 7) {
-      healthSymbol.innerHTML =
-        "&hearts;&hearts;&hearts;&hearts;&hearts;&hearts;&hearts;";
+      healthSymbol.innerHTML = healthCount.repeat(health);
     }
     if (health == 6) {
-      healthSymbol.innerHTML =
-        "&hearts;&hearts;&hearts;&hearts;&hearts;&hearts;";
+      healthSymbol.innerHTML = healthCount.repeat(health);
     }
     if (health == 5) {
-      healthSymbol.innerHTML = "&hearts;&hearts;&hearts;&hearts;&hearts;";
+      healthSymbol.innerHTML = healthCount.repeat(health);
     }
     if (health == 4) {
-      healthSymbol.innerHTML = "&hearts;&hearts;&hearts;&hearts;";
+      healthSymbol.innerHTML = healthCount.repeat(health);
     }
     if (health == 3) {
-      healthSymbol.innerHTML = "&hearts;&hearts;&hearts;";
+      healthSymbol.innerHTML = healthCount.repeat(health);
     }
     if (health == 2) {
-      healthSymbol.innerHTML = "&hearts;&hearts;";
+      healthSymbol.innerHTML = healthCount.repeat(health);
     }
     if (health == 1) {
-      healthSymbol.innerHTML = "&hearts;";
+      healthSymbol.innerHTML = healthCount.repeat(health);
     }
     if (health == 0) {
-      healthSymbol.innerHTML = "&#9760;";
+      healthSymbol.innerHTML = skull.repeat(1);
       healthSymbol.style.fontSize = "100px";
+      healthSymbol.style.marginTop = "65px";
     }
 
     if (health >= 1) {
