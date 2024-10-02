7e14634d5690e1d4eb8f0f14efe8446022080159
Cebrail Kocakafa <cebrail.kocakafa@telefonica.com>
2024-10-02 15:13:54 +0200

    mostlyFinishedGame

--- script.js
+++ script.js
@@ -1,104 +1,104 @@
-let button = document.querySelector(".startButton");
+const body = document.querySelector("body");
+const button = document.querySelector(".startButton");
+let winnerChars = [];
+let underscoreArray = [];
+let result;
+let keyboardArray = [];
+let keyPress; //!!!!
+const gameWord = document.querySelector("#gameWord");
+const win = document.querySelector("#win");
+const keyboard = document.querySelector("#keyboard");
+const healthSymbol = document.querySelector("#health");
+const winLogged = document.querySelector("#winLogged");
+let underscoredArray = [];
+let health = 8;
+let healthCount = "&hearts;";
+let skull = "&#9760;";
+let totalWins = Number(loadTotalWins());
+
+if (saveWins !== 0) {
+  saveWins(totalWins);
+}
 
 button.addEventListener("click", async function () {
-    let url = "https://random-word-api.herokuapp.com/word?lang=de";
-   let word = awaitfetch(url)
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        return data[0];
-      });
-
-  let restartButton = document.querySelector(".startButton");
-  restartButton.textContent = "Restart";
-  let healthSymbol = document.querySelector("#health");
-  let winnerChars = word.split("");
-  let array = [];
-  let keyboardArray = [];
+  health = 8;
+  let url = "https://random-word-api.herokuapp.com/word?lang=de";
+  let randomWord = await fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data[0]);
+      return data[0];
+    });
+  keyboardArray = [];
+  button.textContent = "Restart";
+  winnerChars = randomWord.split("");
+  underscoredArray = [];
   for (let i = 0; i < winnerChars.length; i++) {
-    array.push("_");
+    underscoredArray.push("_");
   }
-  PageTransitionEvent;
-  let keyPress = "H";
-  let result = winnerChars.filter((key) => key == keyPress);
-  let gameWord = document.querySelector("#gameWord");
-  let win = document.querySelector("#win");
-  let keyboard = document.querySelector("#keyboard");
-  console.log(winnerChars);
-  console.log(keyPress);
-  console.log(result);
+  result = winnerChars.filter((key) => key == keyPress);
 
-  gameWord.textContent = array.join(" ");
+  gameWord.textContent = underscoredArray.join(" ");
+});
 
-  const body = document.querySelector("body");
+body.addEventListener("keypress", logKey);
 
-  let healthCount = "&hearts;";
-  let skull = "&#9760;";
-  let health = 8;
-  if ((health = 8)) {
-    healthSymbol.style.fontSize = "25px";
+function logKey(event) {
+  for (let i = 0; i < winnerChars.length; i++) {
+    if (event.key === winnerChars[i]) {
+      underscoredArray[i] = winnerChars[i];
+      gameWord.textContent = underscoredArray.join(" ");
+    }
+  }
+  if (event.key.match(/^[a-z]$/i)) {
+    if (!keyboardArray.includes(event.key)) {
+      keyboardArray.push(event.key);
+    }
+
+    keyboard.textContent =
+      "bereits geraten: [ " + keyboardArray.sort().join(" ") + " ]";
+  }
+  damage(event);
+  checkWin();
+}
+button.addEventListener("click", function reset() {
+  health = 8;
+  healthSymbol.style.fontSize = "25px";
+  healthSymbol.innerHTML = healthCount.repeat(health);
+  win.textContent = "noch 8 Leben";
+});
+
+function checkWin() {
+  if (underscoredArray.join("") == winnerChars.join("")) {
+    win.textContent = "Gewonnen!";
+    totalWins += 1;
+    console.log(typeof totalWins);
+    saveWins(totalWins);
+    winLogged.textContent = "Your total wins: " + totalWins;
+  }
+}
+function damage(event) {
+  if (!winnerChars.includes(event.key)) {
+    console.log((health -= 1));
   }
   if (health == 8) {
+    healthSymbol.style.fontSize = "25px";
     healthSymbol.innerHTML = healthCount.repeat(health);
   }
-  body.addEventListener("keypress", logKey);
-
-  function logKey(event) {
-    for (let i = 0; i < winnerChars.length; i++) {
-      if (event.key === winnerChars[i]) {
-        array[i] = winnerChars[i];
-        gameWord.textContent = array.join(" ");
-      }
-    }
-
-    if (!event.key.match(/^[a-z]$/i)) {
-      keyboardArray.push(event.key);
-      keyboard.textContent =
-        "bereits geraten: [ " + keyboardArray.sort().join(" ") + " ]";
-    }
-
-    if (health == 0) {
-      return "GAMEOVER";
-    }
-
-    if (!winnerChars.includes(event.key)) {
-      console.log((health -= 1));
-    }
-    if (health == 7) {
-      healthSymbol.innerHTML = healthCount.repeat(health);
-    }
-    if (health == 6) {
-      healthSymbol.innerHTML = healthCount.repeat(health);
-    }
-    if (health == 5) {
-      healthSymbol.innerHTML = healthCount.repeat(health);
-    }
-    if (health == 4) {
-      healthSymbol.innerHTML = healthCount.repeat(health);
-    }
-    if (health == 3) {
-      healthSymbol.innerHTML = healthCount.repeat(health);
-    }
-    if (health == 2) {
-      healthSymbol.innerHTML = healthCount.repeat(health);
-    }
-    if (health == 1) {
-      healthSymbol.innerHTML = healthCount.repeat(health);
-    }
-    if (health == 0) {
-      healthSymbol.innerHTML = skull.repeat(1);
-      healthSymbol.style.fontSize = "100px";
-      healthSymbol.style.marginTop = "65px";
-    }
-
-    if (array.join("") == winnerChars.join("")) {
-      healthSymbol.textContent = "Gewonnen!";
-    }
-    if (health >= 1) {
-      win.textContent = "noch " + health + " Leben";
-    }
-    if (health < 1) {
-      win.textContent = "verloren!";
-    }
+  if (health >= 1) {
+    healthSymbol.innerHTML = healthCount.repeat(health);
+    win.textContent = "noch " + health + " Leben";
+  } else {
+    healthSymbol.innerHTML = skull.repeat(1);
+    healthSymbol.style.fontSize = "100px";
+    healthSymbol.style.marginTop = "65px";
+    return (win.textContent = "verloren!");
   }
-});
+}
+
+function saveWins() {
+  localStorage.setItem("storagewin", totalWins);
+}
+function loadTotalWins() {
+  return localStorage.getItem("storagewin");
+}
